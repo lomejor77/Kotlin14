8cdbab0e36b35fc1d8256e099027247da7626b7d
Alex Muñoz <lomejor77@gmail.com>
2023-07-25 11:15:33 -0400

    Ejercicio kotlin 14 agregado diseño y funcionalidades de radiobutton

--- app/src/main/java/cl/awakelabs/kotlin14/MainActivity.kt
+++ app/src/main/java/cl/awakelabs/kotlin14/MainActivity.kt
@@ -2,10 +2,36 @@ package cl.awakelabs.kotlin14
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import cl.awakelabs.kotlin14.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var saldo = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnSelect.setOnClickListener {
+
+            when (binding.radioGroup.checkedRadioButtonId) {
+                R.id.radioSaldo -> binding.textSaldo.setText(saldo.toString())
+                R.id.radioIngreso -> ingresoSaldo()
+                R.id.radioRetiro -> retiroSaldo()
+                R.id.radioExit -> finish()
+            }
+        }
+    }
+
+    private fun retiroSaldo() {
+        saldo -= binding.editMonto.text.toString().toInt()
+        Toast.makeText(this, "Su retiro se ha concretado correctamente", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun ingresoSaldo() {
+        saldo += binding.editMonto.text.toString().toInt()
+        Toast.makeText(this, "Su saldo se ha actualizado correctamente", Toast.LENGTH_SHORT).show()
     }
 }
